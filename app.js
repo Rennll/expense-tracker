2e59e7e393f87d6fb6b3e2c4042d5133882bb862
Rennll <dt616717@gmail.com>
2022-05-27 21:24:08 +0800

    feat: add main layout

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
 
 const PORT = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('This is a expense tracker.')
+  res.render('index')
 })
 
 app.listen(PORT, (req, res) => {
