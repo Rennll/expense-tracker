7201515e4338eaa146002ad8d950aa48ec7bbee8
Rennll <dt616717@gmail.com>
2022-05-27 23:15:20 +0800

    feat: setting  mongoose

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+const db = mongoose.connection
+db.on('error', error => console.log(error))
+db.once('open', () => console.log('MongoDB is connected.'))
 
 const PORT = 3000
 
