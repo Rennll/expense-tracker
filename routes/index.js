69d5b8751f6be71d3ffc4006689af5cd1f02ce0d
Rennll <dt616717@gmail.com>
2022-06-05 00:52:32 +0800

    feat: add login page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const home = require('./modules/home')
 const records = require('./modules/records')
+const users = require('./modules/users')
 
 const router = express()
+router.use('/users', users)
 router.use('/records', records)
 router.use('/', home)
 
