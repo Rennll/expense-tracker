58fc32ef4378b4098b82edc5a94982c528386bbc
Rennll <dt616717@gmail.com>
2022-06-05 01:21:28 +0800

    feat: add register func with user model

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -10,4 +10,28 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.post('/register', async (req, res) => {
+  const { name, email, password, confirmedPassword } = req.body
+  const repeatedEmail = await User.find({ email })
+
+  if (repeatedEmail.length) {
+    return res.render('register', { name, email, password, confirmedPassword })
+  }
+  if (password !== confirmedPassword) {
+    return res.render('register', { name, email, password, confirmedPassword })
+  }
+
+  let id = 0
+  const lastRecord = await User.find().sort({ id: -1 }).limit(1)
+  if (lastRecord.length) id = lastRecord[0].id
+
+  await User.create({
+    id: id + 1,
+    name,
+    email,
+    password
+  })
+  res.redirect('/users/login')
+})
+
 module.exports = router
